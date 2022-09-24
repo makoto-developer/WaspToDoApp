39aa23b5da05fd2638d4a437e971b3269585e093
makoto-developer <72484465+makoto-developer@users.noreply.github.com>
2022-09-24 23:29:50 +0900

    task読み込めるようにする

--- ext/MainPage.js
+++ ext/MainPage.js
@@ -1,10 +1,34 @@
 import React from 'react'
 
+import getTasks from '@wasp/queries/getTasks'
+import {useQuery} from '@wasp/queries'
+
 const MainPage = () => {
+  const { data: tasks, isFetching, error } = useQuery(getTasks)
+
   return (
-    <div>
-      Hello Wasp.
-    </div>
+      <div>
+        {tasks && <TasksList tasks={tasks} />}
+
+        {isFetching && 'Fetching...'}
+        {error && 'Error: ' + error}
+      </div>
   )
 }
+
+const Task = (props) => (
+    <div>
+      <input
+          type='checkbox' id={props.task.id}
+          checked={props.task.isDone} readonly
+      />
+      {props.task.description}
+    </div>
+)
+
+const TasksList = (props) => {
+  if (!props.tasks?.length) return 'No tasks'
+  return props.tasks.map((task, idx) => <Task task={task} key={idx} />)
+}
+
 export default MainPage
